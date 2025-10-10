3158fd0106d13c04ecd58673cfe9cc126b00ce06
Gonzalo Coronado <jgcoronadocueva@outlook.com>
2025-10-10 06:46:45 -0500

    Missing routes added to API documentation

--- L04/routes/temple.js
+++ L04/routes/temple.js
@@ -2,8 +2,11 @@ const routes = require('express').Router();
 const temples = require('../controllers/temple.js');
 
 routes.get('/', temples.findAll);
+routes.get('/published', temples.findAllPublished);
 routes.get('/:temple_id', temples.findOne);
-
 routes.post('/', temples.create);
+routes.put('/:temple_id', temples.update);
+routes.delete('/:temple_id', temples.delete);
+routes.delete('/', temples.deleteAll);
 
 module.exports = routes;
